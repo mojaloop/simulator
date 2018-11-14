9e66036094cc0b71ac2ea5453573896b172c6722
Miguel de Barros <miguel@debarros.me>
2018-11-14 18:11:37 +0000

    Aligned code formatting to ESLint standards, added logger for clarity and also flexibility to disable/enable logging and set log levels as required. Also updated Helm scripts to pass in ILP Packet, Condition, Fulfilment & Log Level as part of Env Params

--- src/payee/handler.js
+++ src/payee/handler.js
@@ -24,13 +24,17 @@
 const cc = require('five-bells-condition')
 const IlpPacket = require('ilp-packet')
 const crypto = require('crypto')
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
-const fetch = require('node-fetch');
+const NodeCache = require('node-cache')
+const myCache = new NodeCache()
+const fetch = require('node-fetch')
+const Logger = require('../lib/logger')
 
-const partiesEndpoint = process.env.PARTIES_ENDPOINT
-const quotesEndpoint = process.env.QUOTES_ENDPOINT
-const transfersEndpoint = process.env.TRANSFERS_ENDPOINT
+const partiesEndpoint = process.env.PARTIES_ENDPOINT || 'http://localhost:1080'
+const quotesEndpoint = process.env.QUOTES_ENDPOINT || 'http://localhost:1080'
+const transfersEndpoint = process.env.TRANSFERS_ENDPOINT || 'http://localhost:1080'
+const transfersFulfilment = process.env.TRANSFERS_FULFILMENT || 'XoSz1cL0tljJSCp_VtIYmPNw-zFUgGfbUqf69AagUzY'
+const transfersCondition = process.env.TRANSFERS_CONDITION || 'HOr22-H3AfTDHrSkPjJtVPRdKouuMkDXTR4ejlQa8Ks'
+const transfersIlpPacket = process.env.TRANSFERS_ILPPACKET || 'AQAAAAAAAADIEHByaXZhdGUucGF5ZWVmc3CCAiB7InRyYW5zYWN0aW9uSWQiOiIyZGY3NzRlMi1mMWRiLTRmZjctYTQ5NS0yZGRkMzdhZjdjMmMiLCJxdW90ZUlkIjoiMDNhNjA1NTAtNmYyZi00NTU2LThlMDQtMDcwM2UzOWI4N2ZmIiwicGF5ZWUiOnsicGFydHlJZEluZm8iOnsicGFydHlJZFR5cGUiOiJNU0lTRE4iLCJwYXJ0eUlkZW50aWZpZXIiOiIyNzcxMzgwMzkxMyIsImZzcElkIjoicGF5ZWVmc3AifSwicGVyc29uYWxJbmZvIjp7ImNvbXBsZXhOYW1lIjp7fX19LCJwYXllciI6eyJwYXJ0eUlkSW5mbyI6eyJwYXJ0eUlkVHlwZSI6Ik1TSVNETiIsInBhcnR5SWRlbnRpZmllciI6IjI3NzEzODAzOTExIiwiZnNwSWQiOiJwYXllcmZzcCJ9LCJwZXJzb25hbEluZm8iOnsiY29tcGxleE5hbWUiOnt9fX0sImFtb3VudCI6eyJjdXJyZW5jeSI6IlVTRCIsImFtb3VudCI6IjIwMCJ9LCJ0cmFuc2FjdGlvblR5cGUiOnsic2NlbmFyaW8iOiJERVBPU0lUIiwic3ViU2NlbmFyaW8iOiJERVBPU0lUIiwiaW5pdGlhdG9yIjoiUEFZRVIiLCJpbml0aWF0b3JUeXBlIjoiQ09OU1VNRVIiLCJyZWZ1bmRJbmZvIjp7fX19'
 
 const extractUrls = (request) => {
   const urls = {}
@@ -46,7 +50,7 @@ const extractUrls = (request) => {
 
 exports.health = function (request, h) {
 
-  return h.response({ status: 'OK' }).code(200)
+  return h.response({status: 'OK'}).code(200)
 }
 
 exports.metadata = function (request, h) {
@@ -57,188 +61,187 @@ exports.metadata = function (request, h) {
 }
 
 exports.postPartiesByTypeAndId = function (request, h) {
-    console.log('IN PAYEEFSP:: POST /payeefsp/parties/'+request.params.id, request.payload)
-    myCache.set(request.params.id, request.payload)
-    return h.response().code(202)
+  Logger.info('IN PAYEEFSP:: POST /payeefsp/parties/' + request.params.id, request.payload)
+  myCache.set(request.params.id, request.payload)
+  return h.response().code(202)
 }
 
 exports.getPartiesByTypeAndId = function (req, h) {
-    (async function () {
-        const metadata = `${req.method} ${req.path} ${req.params.id} `;
-        console.log((new Date().toISOString()), ['IN PAYEEFSP::'], `received: ${metadata}. `);
-        const url = partiesEndpoint+'/parties/MSISDN/' + req.params.id;
-        try {
+  (async function () {
+    const metadata = `${req.method} ${req.path} ${req.params.id} `
+    Logger.info((new Date().toISOString()), ['IN PAYEEFSP::'], `received: ${metadata}. `)
+    const url = partiesEndpoint + '/parties/MSISDN/' + req.params.id
+    try {
 
-            const opts = {
-                method: 'PUT',
-                headers: {
-                    'Accept': 'application/vnd.interoperability.parties+json;version=1',
-                    'Content-Type': 'application/vnd.interoperability.parties+json;version=1.0',
-                    'FSPIOP-Source': 'payeefsp',
-                    'FSPIOP-Destination': req.headers['fspiop-source'],
-                    'Date': req.headers['date']
-                },
-                rejectUnauthorized: false,
-                body: JSON.stringify(myCache.get(req.params.id))
-            };
-            console.log((new Date().toISOString()),`Executing PUT`, url);
-            const res = await fetch(url, opts);
-            console.log((new Date().toISOString()),'response: ', res.status);
-            if (!res.ok) {
-                // TODO: how does one identify the failed response?
-                throw new Error('Failed to send. Result:', res);
-            }
+      const opts = {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/vnd.interoperability.parties+json;version=1',
+          'Content-Type': 'application/vnd.interoperability.parties+json;version=1.0',
+          'FSPIOP-Source': 'payeefsp',
+          'FSPIOP-Destination': req.headers['fspiop-source'],
+          'Date': req.headers['date']
+        },
+        rejectUnauthorized: false,
+        body: JSON.stringify(myCache.get(req.params.id))
+      }
+      Logger.info((new Date().toISOString()), `Executing PUT`, url)
+      const res = await fetch(url, opts)
+      Logger.info((new Date().toISOString()), 'response: ', res.status)
+      if (!res.ok) {
+        // TODO: how does one identify the failed response?
+        throw new Error('Failed to send. Result:', res)
+      }
 
-
-        }
-        catch (err) {
-            console.log(['error'], err);
-
-        }
-    })();
-    return h.response().code(202);
+    }
+    catch (err) {
+      Logger.error(err)
+    }
+  })()
+  return h.response().code(202)
 }
 
 exports.postQuotes = function (req, h) {
-    (async function () {
-        const metadata = `${req.method} ${req.path}`;
-        const quotesRequest = req.payload
-        console.log((new Date().toISOString()),['IN PAYEEFSP::'], `received: ${metadata}. `);
-        console.log('incoming request: ', quotesRequest.quoteId)
+  (async function () {
+    const metadata = `${req.method} ${req.path}`
+    const quotesRequest = req.payload
+    Logger.info((new Date().toISOString()), ['IN PAYEEFSP::'], `received: ${metadata}. `)
+    Logger.info('incoming request: ', quotesRequest.quoteId)
 
-        //prepare response
-        // const fulfillImage = new cc.PreimageSha256()
-        // fulfillImage.setPreimage(new Buffer('hello world'))
-        // console.log(fulfillImage.serializeUri())
-        // console.log(fulfillImage.getConditionUri())
-        // const condition = fulfillImage.getConditionUri()
-        //
-        // const binaryPrepare = IlpPacket.serializeIlpPrepare({
-        //     amount: '10',
-        //     executionCondition: condition,
-        //     destination: 'g.us.nexus.bob', // this field was called 'account' in older packet types
-        //     data: Buffer.from('hello world'),
-        //     expiresAt: new Date(new Date().getTime() + 10000)
-        // })
+    //prepare response
+    // const fulfillImage = new cc.PreimageSha256()
+    // fulfillImage.setPreimage(new Buffer('hello world'))
+    // Logger.info(fulfillImage.serializeUri())
+    // Logger.info(fulfillImage.getConditionUri())
+    // const condition = fulfillImage.getConditionUri()
+    //
+    // const binaryPrepare = IlpPacket.serializeIlpPrepare({
+    //     amount: '10',
+    //     executionCondition: condition,
+    //     destination: 'g.us.nexus.bob', // this field was called 'account' in older packet types
+    //     data: Buffer.from('hello world'),
+    //     expiresAt: new Date(new Date().getTime() + 10000)
+    // })
 
-        const quotesResponse = {
-            transferAmount: {
-                amount: quotesRequest.amount.amount,
-                currency: quotesRequest.amount.currency
-            },
-            payeeFspFee: {
-                amount: '1',
-                currency: quotesRequest.amount.currency
-            },
-            payeeFspCommission: {
-                amount: '1',
-                currency: quotesRequest.amount.currency
-            },
-            expiration: new Date(new Date().getTime() + 10000),
-            ilpPacket: 'AQAAAAAAAABkEHByaXZhdGUucGF5ZWVmc3CCAlV7InRyYW5zYWN0aW9uSWQiOiJhYWUwYzIxMi0wOTJiLTQ5MmItYWQ2ZS1kZmJiYmJjNWRkYzIiLCJxdW90ZUlkIjoiYWFlMGMyMTItMDkyYi00OTJiLWFkNmUtZGZiYmJiYzVkZGMyIiwicGF5ZWUiOnsicGFydHlJZEluZm8iOnsicGFydHlJZFR5cGUiOiJNU0lTRE4iLCJwYXJ0eUlkZW50aWZpZXIiOiIyMjUwNDAwNDc2MiIsImZzcElkIjoicGF5ZWVmc3AifSwicGVyc29uYWxJbmZvIjp7ImNvbXBsZXhOYW1lIjp7fX19LCJwYXllciI6eyJwYXJ0eUlkSW5mbyI6eyJwYXJ0eUlkVHlwZSI6Ik1TSVNETiIsInBhcnR5SWRlbnRpZmllciI6IjI3NzEzODAzOTA1IiwiZnNwSWQiOiJwYXllcmZzcCJ9LCJwZXJzb25hbEluZm8iOnsiY29tcGxleE5hbWUiOnsiZmlyc3ROYW1lIjoiTWF0cyIsImxhc3ROYW1lIjoiSGFnbWFuIn19fSwiYW1vdW50Ijp7ImN1cnJlbmN5IjoiVVNEIiwiYW1vdW50IjoiMTAwIn0sInRyYW5zYWN0aW9uVHlwZSI6eyJzY2VuYXJpbyI6IlRSQU5TRkVSIiwic3ViU2NlbmFyaW8iOiJUUkFOU0ZFUiIsImluaXRpYXRvciI6IlBBWUVSIiwiaW5pdGlhdG9yVHlwZSI6IkNPTlNVTUVSIiwicmVmdW5kSW5mbyI6e319LCJub3RlIjoiaGVqIn0=',
-            condition: '_EVkxF7q3V-XDfIztgcHEa3iTqKHt_zKMV5Yjre_Y_o'
-        }
+    const quotesResponse = {
+      transferAmount: {
+        amount: quotesRequest.amount.amount,
+        currency: quotesRequest.amount.currency
+      },
+      payeeFspFee: {
+        amount: '1',
+        currency: quotesRequest.amount.currency
+      },
+      payeeFspCommission: {
+        amount: '1',
+        currency: quotesRequest.amount.currency
+      },
+      expiration: new Date(new Date().getTime() + 10000),
+      // ilpPacket: 'AQAAAAAAAABkEHByaXZhdGUucGF5ZWVmc3CCAlV7InRyYW5zYWN0aW9uSWQiOiJhYWUwYzIxMi0wOTJiLTQ5MmItYWQ2ZS1kZmJiYmJjNWRkYzIiLCJxdW90ZUlkIjoiYWFlMGMyMTItMDkyYi00OTJiLWFkNmUtZGZiYmJiYzVkZGMyIiwicGF5ZWUiOnsicGFydHlJZEluZm8iOnsicGFydHlJZFR5cGUiOiJNU0lTRE4iLCJwYXJ0eUlkZW50aWZpZXIiOiIyMjUwNDAwNDc2MiIsImZzcElkIjoicGF5ZWVmc3AifSwicGVyc29uYWxJbmZvIjp7ImNvbXBsZXhOYW1lIjp7fX19LCJwYXllciI6eyJwYXJ0eUlkSW5mbyI6eyJwYXJ0eUlkVHlwZSI6Ik1TSVNETiIsInBhcnR5SWRlbnRpZmllciI6IjI3NzEzODAzOTA1IiwiZnNwSWQiOiJwYXllcmZzcCJ9LCJwZXJzb25hbEluZm8iOnsiY29tcGxleE5hbWUiOnsiZmlyc3ROYW1lIjoiTWF0cyIsImxhc3ROYW1lIjoiSGFnbWFuIn19fSwiYW1vdW50Ijp7ImN1cnJlbmN5IjoiVVNEIiwiYW1vdW50IjoiMTAwIn0sInRyYW5zYWN0aW9uVHlwZSI6eyJzY2VuYXJpbyI6IlRSQU5TRkVSIiwic3ViU2NlbmFyaW8iOiJUUkFOU0ZFUiIsImluaXRpYXRvciI6IlBBWUVSIiwiaW5pdGlhdG9yVHlwZSI6IkNPTlNVTUVSIiwicmVmdW5kSW5mbyI6e319LCJub3RlIjoiaGVqIn0=',
+      ilpPacket: transfersIlpPacket,
+      condition: transfersCondition
+      // condition: '_EVkxF7q3V-XDfIztgcHEa3iTqKHt_zKMV5Yjre_Y_o'
+    }
 
-        try {
-            const url = quotesEndpoint+'/quotes/'+quotesRequest.quoteId;
-            const opts = {
-                method: 'PUT',
-                headers: {
-                    'Accept': 'application/vnd.interoperability.quotes+json;version=1',
-                    'Content-Type': 'application/vnd.interoperability.quotes+json;version=1.0',
-                    'FSPIOP-Source': 'payeefsp',
-                    'FSPIOP-Destination': req.headers['fspiop-source'],
-                    'Date': req.headers['date']
-                },
-                rejectUnauthorized: false,
-                body: JSON.stringify(quotesResponse)
-            };
-            console.log((new Date().toISOString()),`Executing PUT`, url);
-            const res = await fetch(url, opts);
-            console.log((new Date().toISOString()),'response: ', res.status);
-            if (!res.ok) {
-                // TODO: how does one identify the failed response?
-                throw new Error('Failed to send. Result:', res);
-            }
+    try {
+      const url = quotesEndpoint + '/quotes/' + quotesRequest.quoteId
+      const opts = {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/vnd.interoperability.quotes+json;version=1',
+          'Content-Type': 'application/vnd.interoperability.quotes+json;version=1.0',
+          'FSPIOP-Source': 'payeefsp',
+          'FSPIOP-Destination': req.headers['fspiop-source'],
+          'Date': req.headers['date']
+        },
+        rejectUnauthorized: false,
+        body: JSON.stringify(quotesResponse)
+      }
+      Logger.info((new Date().toISOString()), `Executing PUT`, url)
+      const res = await fetch(url, opts)
+      Logger.info((new Date().toISOString()), 'response: ', res.status)
+      if (!res.ok) {
+        // TODO: how does one identify the failed response?
+        throw new Error('Failed to send. Result:', res)
+      }
 
-
-        }
-        catch (err) {
-            console.log(['error'], err);
-            // TODO: what if this fails? We need to log. What happens by default?
-            //const url = await rq.createErrorUrl(db, req.path, requesterName);
-            // TODO: review this error message
-            // TODO: we should be able to throw an AppError somewhere, test whether the error
-            // received in this handler is an AppError, then send the requester the correct
-            // payload etc. based on the contents of that AppError.
-            //rq.sendError(url, asyncResponses.serverError, rq.defaultHeaders(requesterName, 'participants'), {logger});
-        }
-    })();
-    return h.response().code(202);
+    }
+    catch (err) {
+      Logger.error(err)
+      // TODO: what if this fails? We need to log. What happens by default?
+      //const url = await rq.createErrorUrl(db, req.path, requesterName);
+      // TODO: review this error message
+      // TODO: we should be able to throw an AppError somewhere, test whether the error
+      // received in this handler is an AppError, then send the requester the correct
+      // payload etc. based on the contents of that AppError.
+      //rq.sendError(url, asyncResponses.serverError, rq.defaultHeaders(requesterName, 'participants'), {logger});
+    }
+  })()
+  return h.response().code(202)
 }
 
 exports.postTransfers = function (req, h) {
-    (async function () {
-        const metadata = `${req.method} ${req.path} ${req.payload.transferId}`;
-        console.log((new Date().toISOString()),['IN PAYEEFSP::'], `received: ${metadata}. `);
-        const url = transfersEndpoint+'/transfers/' + req.payload.transferId;
-        try {
+  (async function () {
+    const metadata = `${req.method} ${req.path} ${req.payload.transferId}`
+    Logger.info(`IN PAYEEFSP:: received: ${metadata}.`)
+    const url = transfersEndpoint + '/transfers/' + req.payload.transferId
+    try {
 
-            const transfersResponse = {
-                fulfilment: "rjzWyHf4IUao60Yz98HZOIhZbqtclOgZ7WriZuq9Hn0",
-                completedTimestamp: new Date().toISOString(),
-                transferState: "COMMITTED"
-            }
+      const transfersResponse = {
+        // fulfilment: "rjzWyHf4IUao60Yz98HZOIhZbqtclOgZ7WriZuq9Hn0",
+        fulfilment: transfersFulfilment,
+        completedTimestamp: new Date().toISOString(),
+        transferState: 'COMMITTED'
+      }
 
-            const opts = {
-                method: 'PUT',
-                headers: {
-                    'Accept': 'application/vnd.interoperability.transfers+json;version=1',
-                    'Content-Type': 'application/vnd.interoperability.transfers+json;version=1.0',
-                    'FSPIOP-Source': 'payeefsp',
-                    'FSPIOP-Destination': req.headers['fspiop-source'],
-                    'Date': req.headers['date']
-                },
-                rejectUnauthorized: false,
-                body: JSON.stringify(transfersResponse)
-            };
-            console.log((new Date().toISOString()),`Executing PUT`, url, 'with body:', JSON.stringify(transfersResponse));
-            const res = await fetch(url, opts);
-            console.log((new Date().toISOString()),'response: ', res.status);
-            if (!res.ok) {
-                // TODO: how does one identify the failed response?
-                throw new Error('Failed to send. Result:', res);
-            }
+      const opts = {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/vnd.interoperability.transfers+json;version=1',
+          'Content-Type': 'application/vnd.interoperability.transfers+json;version=1.0',
+          'FSPIOP-Source': req.headers['fspiop-destination'],
+          'FSPIOP-Destination': req.headers['fspiop-source'],
+          'Date': req.headers['date']
+        },
+        rejectUnauthorized: false,
+        body: JSON.stringify(transfersResponse)
+      }
+      Logger.info(`Executing PUT: [${url}], HEADERS: [${JSON.stringify(opts.headers)}], BODY: [${JSON.stringify(transfersResponse)}]`)
+      const res = await fetch(url, opts)
+      Logger.info(`response: ${res.status}`)
+      if (!res.ok) {
+        // TODO: how does one identify the failed response?
+        throw new Error('Failed to send. Result:', res)
+      }
 
-
-        }
-        catch (err) {
-            console.log(['error'], err);
-            // TODO: what if this fails? We need to log. What happens by default?
-            //const url = await rq.createErrorUrl(db, req.path, requesterName);
-            // TODO: review this error message
-            // TODO: we should be able to throw an AppError somewhere, test whether the error
-            // received in this handler is an AppError, then send the requester the correct
-            // payload etc. based on the contents of that AppError.
-            //rq.sendError(url, asyncResponses.serverError, rq.defaultHeaders(requesterName, 'participants'), {logger});
-        }
-    })();
-    return h.response().code(202);
+    }
+    catch (err) {
+      Logger.error(err)
+      // TODO: what if this fails? We need to log. What happens by default?
+      //const url = await rq.createErrorUrl(db, req.path, requesterName);
+      // TODO: review this error message
+      // TODO: we should be able to throw an AppError somewhere, test whether the error
+      // received in this handler is an AppError, then send the requester the correct
+      // payload etc. based on the contents of that AppError.
+      //rq.sendError(url, asyncResponses.serverError, rq.defaultHeaders(requesterName, 'participants'), {logger});
+    }
+  })()
+  return h.response().code(202)
 }
 
 exports.putTransfersById = function (request, h) {
-    console.log((new Date().toISOString()),'IN PAYEEFSP:: PUT /payeefsp/transfers/'+request.params.id, request.payload)
-    myCache.set(request.params.id, request.payload)
-    return h.response().code(200)
+  Logger.info(`IN PAYEEFSP:: PUT /payeefsp/transfers/${request.params.id}, PAYLOAD: [${JSON.stringify(request.payload)}]`)
+  myCache.set(request.params.id, request.payload)
+  return h.response().code(200)
 }
 
 exports.putTransfersByIdError = function (request, h) {
-    console.log('IN PAYEEFSP:: PUT /payeefsp/transfers/'+request.params.id+'/error', request.payload)
-    myCache.set(request.params.id, request.payload)
-    return h.response().code(200)
+  Logger.info(`IN PAYEEFSP:: PUT /payeefsp/transfers/${request.params.id}/error, PAYLOAD: [${JSON.stringify(request.payload)}]`)
+  myCache.set(request.params.id, request.payload)
+  return h.response().code(200)
 }
 
 exports.getcorrelationId = function (request, h) {
-    console.log((new Date().toISOString()),'IN PAYEEFSP:: Final response for GET /payeefsp/correlationid/'+request.params.id, myCache.get(request.params.id))
-    return h.response(myCache.get(request.params.id)).code(202)
+  Logger.info(`IN PAYEEFSP:: Final response for GET /payeefsp/correlationid/${request.params.id}, CACHE: [${JSON.stringify(myCache.get(request.params.id))}`)
+  return h.response(myCache.get(request.params.id)).code(202)
 }
