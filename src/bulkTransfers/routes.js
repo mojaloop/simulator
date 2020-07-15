66c74c509c6a5b363abb8e37a0e9977c0517c584
Steven Oderayi <oderayi@gmail.com>
2020-07-15 12:37:39 +0100

    Bugfix/#1375: Fix payload spec for bulk transfer error endpoint (#206)
    * Fix bulk transfers error endpoint payload spec
    
    * Fix bulk transfers error endpoint payload spec

--- src/bulkTransfers/routes.js
+++ src/bulkTransfers/routes.js
@@ -180,8 +180,16 @@ module.exports = [
           id: Joi.string().required().description('path')
         }),
         payload: Joi.object({
-          bulkTransferState: Joi.string().required().valid('RECEIVED', 'PENDING', 'ACCEPTED', 'PROCESSING', 'COMPLETED', 'REJECTED').description('State of the bulk transfer').label('@ Invalid bulk transfer state given. @'),
-          completedTimestamp: Joi.string().regex(/^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:(\.\d{3}))(?:Z|[+-][01]\d:[0-5]\d)$/).description('When the transfer was completed').label('@ A valid transfer completion date must be supplied. @')
+          errorInformation: Joi.object().keys({
+            errorDescription: Joi.string().required(),
+            errorCode: Joi.string().required().regex(/^[0-9]{4}/),
+            extensionList: Joi.object().keys({
+              extension: Joi.array().items(Joi.object().keys({
+                key: Joi.string().required().min(1).max(32).description('Key').label('@ Supplied key fails to match the required format. @'),
+                value: Joi.string().required().min(1).max(128).description('Value').label('@ Supplied key value fails to match the required format. @')
+              })).required().min(1).max(16).description('extension')
+            }).optional().description('Extension list')
+          }).required().description('Error information')
         })
       }
     }
