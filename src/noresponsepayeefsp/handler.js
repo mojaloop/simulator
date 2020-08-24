1dca2d9b98ee21b25e4d39ee81f5b0abb7c6d5a4
Steven Oderayi <oderayi@gmail.com>
2020-08-24 13:59:39 +0100

    Add enpoints to support unhappy path testing for quoting service

--- src/noresponsepayeefsp/handler.js
+++ src/noresponsepayeefsp/handler.js
@@ -131,3 +131,66 @@ exports.getCallbackById = function (request, h) {
 
   return h.response(responseData).code(Enums.Http.ReturnCodes.OK.CODE)
 }
+
+exports.postQuotes = function (request, h) {
+  (async function () {
+    const histTimerEnd = Metrics.getHistogram(
+      'sim_request',
+      'Histogram for Simulator http operations',
+      ['success', 'fsp', 'operation', 'source', 'destination']
+    ).startTimer()
+
+    const metadata = `${request.method} ${request.path}`
+    const quoteRequest = request.payload
+    Logger.isInfoEnabled && Logger.info((new Date().toISOString()), ['IN NORESPONSEPAYEEFSP::'], `received: ${metadata}. `)
+    Logger.isInfoEnabled && Logger.info(`incoming request: ${quoteRequest.quoteId}`)
+
+    // Saving Incoming request
+    const incomingRequest = {
+      headers: request.headers,
+      data: quoteRequest
+    }
+    requestCache.set(quoteRequest.quoteId, incomingRequest)
+    histTimerEnd({ success: true, fsp: 'payer', operation: 'postQuotes', source: request.headers['fspiop-source'], destination: request.headers['fspiop-destination'] })
+  })()
+  return h.response().code(Enums.Http.ReturnCodes.ACCEPTED.CODE)
+}
+
+exports.putQuotesById = function (request, h) {
+  const histTimerEnd = Metrics.getHistogram(
+    'sim_request',
+    'Histogram for Simulator http operations',
+    ['success', 'fsp', 'operation', 'source', 'destination']
+  ).startTimer()
+
+  Logger.isInfoEnabled && Logger.info(`IN NORESPONSEPAYEEFSP:: PUT /noresponsepayeefsp/quotes/${request.params.id}, PAYLOAD: [${JSON.stringify(request.payload)}]`)
+
+  const incomingRequest = {
+    headers: request.headers,
+    data: request.payload
+  }
+  callbackCache.set(request.params.id, incomingRequest)
+  correlationCache.set(request.params.id, request.payload)
+
+  histTimerEnd({ success: true, fsp: 'payee', operation: 'putQuotesById', source: request.headers['fspiop-source'], destination: request.headers['fspiop-destination'] })
+  return h.response().code(Enums.Http.ReturnCodes.OK.CODE)
+}
+
+exports.putQuotesByIdAndError = function (request, h) {
+  console.log((new Date().toISOString()), 'IN NORESPONSEPAYEEFSP:: PUT /noresponsepayeefsp/quotes/' + request.params.id + '/error', request.payload)
+  correlationCache.set(request.params.id, request.payload)
+
+  const incomingRequest = {
+    headers: request.headers,
+    data: request.payload
+  }
+  callbackCache.set(request.params.id, incomingRequest)
+
+  return h.response().code(Enums.Http.ReturnCodes.OK.CODE)
+}
+
+exports.getQuotesById = function (request, h) {
+  Logger.isInfoEnabled && Logger.info(`IN NORESPONSEPAYEEFSP:: GET /noresponsepayeefsp/quotes/${request.params.id}, CACHE: [undefined]`)
+
+  return h.response().code(Enums.Http.ReturnCodes.ACCEPTED.CODE)
+}
